01f36a26c1dba10b63d19935da8f42cd7a7af79f
Krishanu Das <mgmtcafe@gmail.com>
2018-03-21 16:46:54 +0530

    updated app.js

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -44,7 +44,10 @@ function loginCtrl($scope, $http, $httpParamSerializer) {
 	        data: $httpParamSerializer($scope.data)
 	    }
 	    $http(config).then(function(data){
-	    	console.log(data.error);
+	    	console.log(data.access_token);
+	    },
+	    function(error){
+	    	console.log(data.error)
 	    }); 
 	    
 	    
