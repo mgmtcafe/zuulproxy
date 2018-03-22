a25586021235cfdbe62c9541a43b453d6d695045
mgmtcafe <36656043+mgmtcafe@users.noreply.github.com>
2018-03-22 11:01:41 +0530

    Update SecurityConfiguration.java

--- src/main/java/com/gateway/security/SecurityConfiguration.java
+++ src/main/java/com/gateway/security/SecurityConfiguration.java
@@ -54,14 +54,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        /*http.authorizeRequests().antMatchers("/uaa/**", "/login").permitAll().anyRequest().authenticated()
-            .and()
+        http.authorizeRequests()
+        	.antMatchers("/")
+    		.permitAll().and()
             .csrf().requireCsrfProtectionMatcher(csrfRequestMatcher()).csrfTokenRepository(csrfTokenRepository())
             .and()
             .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
             .addFilterAfter(oAuth2AuthenticationProcessingFilter(), AbstractPreAuthenticatedProcessingFilter.class)
             .logout().permitAll()
-            .logoutSuccessUrl("/");*/
+            .logoutSuccessUrl("/");
     }
 
     private OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter() {
